9a9577430e476532a2618bb9408dec662980ab71
rehna-jp <jeremyrehna05@gmail.com>
2026-09-19 04:11:12 +0000

    Add has_receipt flag and InvalidReceiptAta error for the NFT receipt

--- programs/fundraiser/src/state/contributor.rs
+++ programs/fundraiser/src/state/contributor.rs
@@ -4,4 +4,7 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 pub struct Contributor {
     pub amount: u64,
+    // Set once the one-time NFT receipt has been minted for this contributor,
+    // so a second contribution to the same fundraiser doesn't mint a second one.
+    pub has_receipt: bool,
 }
